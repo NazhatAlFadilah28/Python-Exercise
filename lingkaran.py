242549b864d09c1922e49fb17df04985b33347df
= <=nazhaf2702@gmail.com>
2024-10-01 11:30:19 +0700

    create aplikasi segitiga

---
+++ lingkaran.py
@@ -0,0 +1,10 @@
+print("="*30)
+print("RUMUS KELILING LINGKARAN")
+print("="*30)
+
+PHI = 3.14
+r = int(input("MASUKAN NILAI r : "))
+
+K = 2 * PHI * r
+
+print("HASIL : ",round(K))
---
+++ segitiga.py
@@ -0,0 +1,11 @@
+print("="*30)
+print("RUMUS KELILING SEGITIGA")
+print("="*30)
+
+a = int(input("MASUKAN NILAI SISI 1 : "))
+b = int(input("MASUKAN NILAI SISI 2 : "))
+c =int (input("MASUKAN NILAI SISI 3 : "))
+
+K = a + b + c
+
+print("HASIL : ",K)
